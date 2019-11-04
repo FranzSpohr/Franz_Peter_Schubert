580fd8109f4274dadba51170d45b53471d6adda1
FranzSpohr <FranzSpohr@users.noreply.github.com>
2019-11-03 19:36:15 -0500

    HOPE IT GOES THROUGH

--- src/js/reader.js
+++ src/js/reader.js
@@ -57,7 +57,7 @@ function overlayOn() {
 	// determines whether the Slate tab or student being displayed has changed. If changed, deletes existing HTML elements and creates new ones
     if(activeTab !== targetTab || activeApp !== targetApp) {
       while (overlay.firstChild) {
-	    // necessary to prevent unused HTML elements from cluttering the page
+        // necessary to prevent unused HTML elements from cluttering the page
         overlay.removeChild(overlay.firstChild);
       }
       addElements(imageLink, startPage, endPage, currentPage);
@@ -191,10 +191,10 @@ function toggleZoom(mouseButton) {
   if(mouseButton == 'left') {
     zoomLevel++;
     overlay.style.cursor = 'zoom-in'
-    if (zoomLevel >= zLevels.length) {zoomLevel = 0};
+    if (zoomLevel >= zLevels.length) {zoomLevel = 0}
   } else if (mouseButton == 'right') {
     overlay.style.cursor = 'zoom-in'
-    if (zoomLevel == 0) {overlayOff()} else {zoomLevel--};
+    if (zoomLevel == 0) {overlayOff()} else {zoomLevel--}
   }
   element.setAttribute('style', 'width:' + zLevels[zoomLevel] + '%');
 }
@@ -285,8 +285,8 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlidesUMich');
   var dots = document.getElementsByClassName("dotUMich");
-  if (n > slides.length) {slideIndex = 1};
-  if (n < 1) {slideIndex = slides.length};
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
@@ -297,111 +297,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += ' activeUMich';
   pageNumber.innerHTML = pageNumber.innerHTML.replace(/^[^\d]*(\d+)/, 'Page ' + slideIndex);
 }
-
-// literally just copy pasted code from asvd's dragscroll library. Div overlayUMmich is assigned ID of dragscroll
-(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['exports'], factory);
-  } else if (typeof exports !== 'undefined') {
-    factory(exports);
-  } else {
-    factory((root.dragscroll = {}));
-  }
-} (this, function (exports) {
-  var _window = window;
-  var _document = document;
-  var mousemove = 'mousemove';
-  var mouseup = 'mouseup';
-  var mousedown = 'mousedown';
-  var mouseenter = 'mouseenter';
-  var click = 'click';
-  var EventListener = 'EventListener';
-  var addEventListener = 'add'+EventListener;
-  var removeEventListener = 'remove'+EventListener;
-  var newScrollX, newScrollY;
-  var moveThreshold = 4;
-  var dragged = [];
-  var reset = function(i, el) {
-    for (i = 0; i < dragged.length;) {
-      el = dragged[i++];
-      el = el.container || el;
-      el[removeEventListener](mousedown, el.md, 0);
-      el[removeEventListener](click, el.mc, 0);
-      _window[removeEventListener](mouseup, el.mu, 0);
-      _window[removeEventListener](mousemove, el.mm, 0);
-      _document[removeEventListener](mouseenter, el.me, 0);
-    }
-
-    // cloning into array since HTMLCollection is updated dynamically
-    dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
-    for (i = 0; i < dragged.length;) {
-      (function(el, lastClientX, lastClientY, startX, startY, moved, pushed, scroller, cont){
-        (cont = el.container || el)[addEventListener](
-          mousedown,
-          cont.md = function(e) {
-            if (!el.hasAttribute('nochilddrag') ||
-                _document.elementFromPoint(
-              e.pageX, e.pageY
-            ) == cont
-               ) {
-              pushed = 1;
-              moved = 0;
-              startX = lastClientX = e.clientX;
-              startY = lastClientY = e.clientY;
-              hideTooltip(); //change from original script, hides tooltip on mouse movement
-              e.preventDefault();
-              e.stopPropagation();
-            }
-          }, 0
-        );
-        (cont = el.container || el)[addEventListener](
-          click,
-          cont.mc = function(e) {
-            if (moved) {
-              e.preventDefault();
-              e.stopPropagation();
-              moved = 0; pushed = 0;
-            }
-          }, 1
-        );
-        _window[addEventListener](
-          mouseup, cont.mu = function() {pushed = 0;}, 0
-        );
-        _document[addEventListener](
-          mouseenter, cont.me = function(e) {if (!e.buttonsPressed) pushed = 0;}, 0
-        );
-        _window[addEventListener](
-          mousemove,
-          cont.mm = function(e) {
-            if (pushed) {
-              hideElements(); // another addition to original script, hides dots while scrolling
-              if (!moved &&
-                  (Math.abs(e.clientX - startX) > moveThreshold ||
-                   Math.abs(e.clientY - startY) > moveThreshold)) {
-                moved = true;
-              }
-              if (moved) {
-                (scroller = el.scroller||el).scrollLeft -=
-                  newScrollX = (- lastClientX + (lastClientX=e.clientX));
-                scroller.scrollTop -=
-                  newScrollY = (- lastClientY + (lastClientY=e.clientY));
-                if (el == _document.body) {
-                  (scroller = _document.documentElement).scrollLeft -= newScrollX;
-                  scroller.scrollTop -= newScrollY;
-                }
-              }
-            }
-          }, 0
-        );
-      })(dragged[i++]);
-    }
-  }
-
-  if (_document.readyState == 'complete') {
-    reset();
-  } else {
-    _window[addEventListener]('load', reset, 0);
-  }
-
-  exports.reset = reset;
-}));
