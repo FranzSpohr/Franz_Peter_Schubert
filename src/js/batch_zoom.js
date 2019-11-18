34ec7da22a448cb6c6b3584601b0658d90d95eff
FranzSpohr <FranzSpohr@users.noreply.github.com>
2019-11-18 16:59:35 -0500

    streamlined code and fixed issue with keypress

--- src/js/batch_zoom.js
+++ src/js/batch_zoom.js
@@ -1,10 +1,6 @@
-// stores current zoom level
-var zoomCount = 0;
-// stores whether event listeners were added
-var ListenerAdded = false
-// "z" value that Slate requires to determine size of the document render
-var zoom_Levels = [72, 108, 144, 180, 216];
-
+var zoomCount = 0; // stores current zoom level
+var ListenerAdded = false // stores whether event listeners were added
+var zoom_Levels = [72, 108, 144, 180, 216]; // "z" value that Slate requires to determine size of the document render
 
 const parentElement = window.document;
 const mutationConfig = {
@@ -15,49 +11,17 @@ const mutationConfig = {
   characterDataOldValue: true
 };
 
-var onMutate = function () {
-  if (document.getElementById('batch_pages') !== null) {
+var onMutate = () => {
+  if (document.getElementById('batch_pages') !== null && ListenerAdded == false) {
     var docWindow = document.getElementById('batch_pages');
-    docWindow.addEventListener('keydown', Toggle_Zoom, true);
     docWindow.addEventListener('load', add_Listener, true);
   }
 };
 
 var observer = new MutationObserver(onMutate);
-observer.observe(window.document, mutationConfig);
+observer.observe(parentElement.body, mutationConfig);
 
-// toggles between zoom levels
-function Toggle_Zoom(event) {
-  if (event.code == 'NumpadAdd' || event.code == 'Equal' || event.type == 'click') {
-    event.preventDefault();
-    if (zoomCount == 4) {
-      Click_Zoomer();
-      return;
-    }
-    // selects image elements loaded by batch acquire
-    const elements = document.querySelectorAll('.batch_page_container > img');
-    // replaces the existing "z" value in the URL of documents
-    elements.forEach(function (el) {
-      if (el.src.includes(`z=${zoom_Levels[zoomCount]}`)) {
-        el.src = el.src.replace(`z=${zoom_Levels[zoomCount]}`, `z=${zoom_Levels[zoomCount + 1]}`);
-      }
-    });
-    zoomCount++;
-    Click_Zoomer();
-  } else if (event.code == 'NumpadSubtract' || event.code == 'Minus' || event.type == 'contextmenu') {
-    event.preventDefault();
-    if (zoomCount == 0) {
-      return
-    };
-    const elements = document.querySelectorAll('.batch_page_container > img');
-    elements.forEach(function (el) {
-      if (el.src.includes(`z=${zoom_Levels[zoomCount]}`)) {
-        el.src = el.src.replace(`z=${zoom_Levels[zoomCount]}`, `z=${zoom_Levels[zoomCount - 1]}`);
-      }
-    });
-    zoomCount--;
-  }
-}
+parentElement.addEventListener('keypress', batchZoom, true)
 
 // adds event listeners needed for userscript to function
 function add_Listener() {
@@ -66,36 +30,64 @@ function add_Listener() {
   } else {
     // grabs images and attaches listeners
     const elements = document.querySelectorAll('.batch_page_container > img');
-    elements.forEach(function (el) {
-      el.addEventListener('click', Toggle_Zoom, true)
-      el.addEventListener('contextmenu', Toggle_Zoom, true)
+    elements.forEach(el => {
+      el.addEventListener('click', batchZoom, true)
+      el.addEventListener('contextmenu', batchZoom, true)
     });
     // needed to determine whether "next" buttons, etc. are pressed, meaning listeners have to be attached again
     const buttons = document.querySelectorAll('button[type="button"]');
-    buttons.forEach(function (el) {
-      el.addEventListener('click', function () {
+    buttons.forEach(el => {
+      el.addEventListener('click', () => {
         zoomCount = 0
         ListenerAdded = false
       })
     });
-    !ListenerAdded
+    ListenerAdded = true
   }
 }
 
-/* kinda janky way to automatically close the useless magnifying glass that Slate has by
-immediately simulating a left mouse click to close the magnifier*/
-function Click_Zoomer() {
+// toggles between zoom levels
+function batchZoom(event) {
+  if (parentElement.activeElement.nodeName == 'INPUT') {
+    return
+  } else {
+    if (event.code == 'NumpadAdd' || event.code == 'Equal' || event.type == 'click') {
+      event.preventDefault();
+      if (zoomCount == 4) {
+        hideZoomer();
+        return;
+      }
+      // selects image elements loaded by batch acquire
+      const elements = document.querySelectorAll('.batch_page_container > img');
+
+      // replaces the existing "z" value in the URL of documents
+      elements.forEach(el => {
+        if (el.src.includes(`z=${zoom_Levels[zoomCount]}`)) {
+          el.src = el.src.replace(`z=${zoom_Levels[zoomCount]}`, `z=${zoom_Levels[zoomCount + 1]}`);
+        }
+      });
+      zoomCount++;
+      hideZoomer();
+    } else if (event.code == 'NumpadSubtract' || event.code == 'Minus' || event.type == 'contextmenu') {
+      event.preventDefault();
+      if (zoomCount == 0) {
+        return
+      };
+      const elements = document.querySelectorAll('.batch_page_container > img');
+      elements.forEach(el => {
+        if (el.src.includes(`z=${zoom_Levels[zoomCount]}`)) {
+          el.src = el.src.replace(`z=${zoom_Levels[zoomCount]}`, `z=${zoom_Levels[zoomCount - 1]}`);
+        }
+      });
+      zoomCount--;
+    }
+  }
+}
+
+/* kinda janky way to automatically close Slate's useless magnifying glass thingy*/
+function hideZoomer() {
   var targetNode = document.getElementsByClassName('batch_zoomer boxshadow')[0];
   if (targetNode) {
-    //--- Simulate a natural mouse-click sequence.
-    triggerMouseEvent(targetNode, "mousedown");
-    triggerMouseEvent(targetNode, "mouseup");
-    triggerMouseEvent(targetNode, "click");
-  }
-
-  function triggerMouseEvent(node, eventType) {
-    var clickEvent = document.createEvent('MouseEvents');
-    clickEvent.initEvent(eventType, true, true);
-    node.dispatchEvent(clickEvent);
+    targetNode.parentNode.removeChild(targetNode)
   }
 }
