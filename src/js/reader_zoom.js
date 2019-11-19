14b381a2467846bef1619d519a3d5f854cc53db5
FranzSpohr <FranzSpohr@users.noreply.github.com>
2019-11-19 16:57:19 -0500

    formatting changes
    Changed formatting and cleaned up some confusing variable names.

--- src/js/reader_zoom.js
+++ src/js/reader_zoom.js
@@ -2,9 +2,44 @@ var zoomLevel = 0; // zoom level of a page
 var slideIndex = 1; // which page to display in viewer
 var imageLoaded = false; // toggled when higher DPI images are loaded
 var activeTab; // Slate tab that the images were loaded from
-var activeApp // app currently being displayed
+var activeApp; // app currently being displayed
 var tooltipTimer; // for the timer that times out the tooltip after 15 seconds
 var pageNumber; // stores the element containing page numbers
+
+// injects button into the footer of Slate Reader. clicking it will open the viewer.
+var buttonShow = document.createElement('input');
+buttonShow.type = 'button';
+buttonShow.id = 'buttonSchubert';
+buttonShow.value = 'Display Larger Images';
+buttonShow.disabled = true;
+buttonShow.onclick = overlayOn;
+document.getElementsByClassName('reader_footer')[0].appendChild(buttonShow);
+
+const parentElement = window.document;
+const mutationConfig = {
+  attributes: true,
+  childList: true,
+  subtree: true,
+  characterData: true,
+  characterDataOldValue: true,
+  attributeFilter: ['class']
+};
+
+var onMutate = () => {
+  if (
+    document.querySelector(
+      'body > div.reader_viewer.reader_scrollable > div > div.container.active.loaded > div > img'
+    ) !== null
+  ) {
+    console.log('loaded');
+    buttonShow.disabled = false;
+  } else {
+    buttonShow.disabled = true;
+  }
+};
+
+var observer = new MutationObserver(onMutate);
+observer.observe(parentElement.body, mutationConfig);
 
 // creates an overlay that serves as a canvas for all elements created by this userscript
 var overlay = document.createElement('div');
@@ -20,31 +55,30 @@ overlay.addEventListener('wheel', hideTooltip, {
   passive: false
 });
 overlay.className = 'dragscroll'; // enables scrolling by mouse drag
-overlay.tabIndex = -1; // enables keyboard controls by setting focus on the overlay
-
-// injects button into the footer of Slate Reader. clicking it will open the viewer.
-var input = document.createElement('input');
-input.type = 'button';
-input.id = 'buttonSchubert';
-input.value = 'Display Larger Images';
-input.onclick = overlayOn;
-document.getElementsByClassName('reader_footer')[0].appendChild(input);
+overlay.tabIndex = -1; // enables keyboard controls by setting focus on the overla
 
 // calls overlay and displays higher DPI images
 function overlayOn() {
   // needs to be loaded first to determine whether the current Slate tab has any images or not, displays alert if no images available
-  const imageLink = document.querySelector('body > div.reader_viewer.reader_scrollable > div > div.container.active.loaded > div > img');
+  const imageLink = document.querySelector(
+    'body > div.reader_viewer.reader_scrollable > div > div.container.active.loaded > div > img'
+  );
   if (imageLink == null) {
     alert('Navigate to a tab with documents first.');
     return;
   }
   // uses regular expressions to extract data needed to determine the number of needed new HTML elements
   var startPage = 1;
-  var currentPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+/);
-  var endPage = document.getElementsByClassName('reader_status')[0].childNodes[0].textContent.match(/\d+(?=,)/);
+  var currentPage = document
+    .getElementsByClassName('reader_status')[0]
+    .childNodes[0].textContent.match(/\d+/);
+  var endPage = document
+    .getElementsByClassName('reader_status')[0]
+    .childNodes[0].textContent.match(/\d+(?=,)/);
 
   // determines which Slate tab is currently being displayed
-  var targetApp = document.getElementsByClassName('reader_header_title')[0].innerHTML;
+  var targetApp = document.getElementsByClassName('reader_header_title')[0]
+    .innerHTML;
   var targetTab = document.getElementsByClassName('stream active')[0].innerHTML;
 
   if (imageLoaded) {
@@ -71,10 +105,12 @@ function overlayOn() {
 
 // adds HTML elements needed for the userscript to function
 function addElements(imageSrc, startPg, endPg, currPg) {
-  var iframeUM = document.getElementsByTagName('iframe')[0].contentWindow.document.body; // iframe declared to access student info
+  var iframeUM = document.getElementsByTagName('iframe')[0].contentWindow
+    .document.body; // iframe declared to access student info
   var table = iframeUM.getElementsByClassName('grey')[0]; // declares table containing UMID
   var appName = iframeUM.getElementsByClassName('fullname')[0].innerHTML; // student name
-  var appID = table.rows[1].cells[0].innerHTML + " " + table.rows[1].cells[1].innerHTML; // student UMID
+  var appID =
+    table.rows[1].cells[0].innerHTML + ' ' + table.rows[1].cells[1].innerHTML; // student UMID
 
   var studentInfo = document.createElement('div');
   studentInfo.id = 'studentSchubert';
@@ -88,7 +124,7 @@ function addElements(imageSrc, startPg, endPg, currPg) {
   openTooltip.onclick = displayTooltip;
   document.getElementById('overlaySchubert').appendChild(openTooltip);
 
-  // page counter on the upper right corner, does not need to be looped? Fixed 
+  // page counter on the upper right corner, does not need to be looped? Fixed
   var pgCounter = document.createElement('div');
   pgCounter.id = 'numbertextSchubert';
   pgCounter.innerHTML = 'Page ' + currPg + ' of ' + endPg;
@@ -135,22 +171,23 @@ function addElements(imageSrc, startPg, endPg, currPg) {
 
     var imageElement = document.createElement('img'); // element for higher DPI images of the documents
     var imageNewSrc = imageSrc.src.replace(/z=\d*/, 'z=300'); // replaces the part of URL that specifies DPI of the image
-    var errorDPI = 200 // lowers requested DPI if image fails to be loaded
+    var errorDPI = 200; // lowers requested DPI if image fails to be loaded
 
     // modifies the page number component of the URL to attach correct pages to the slides
-    imageNewSrc = imageNewSrc.replace(/pg=\d*/, `pg=${i-1}`);
+    imageNewSrc = imageNewSrc.replace(/pg=\d*/, `pg=${i - 1}`);
     imageElement.id = 'image_' + i;
     imageElement.src = imageNewSrc;
     imageElement.style.width = '100%';
     // if image cannot be loaded due to request DPI being too high, lowers DPI by 10 until loading is succesful
     imageElement.onerror = () => {
       errorDPI -= 10;
-      this.src = this.src.replace(/z=\d*/, `z=${errorDPI}`)
+      this.src = this.src.replace(/z=\d*/, `z=${errorDPI}`);
     };
     document.getElementById('slide_' + i).appendChild(imageElement);
   }
   activeTab = document.getElementsByClassName('stream active')[0].innerHTML;
-  activeApp = document.getElementsByClassName('reader_header_title')[0].innerHTML;
+  activeApp = document.getElementsByClassName('reader_header_title')[0]
+    .innerHTML;
   pageNumber = document.getElementById('numbertextSchubert');
   zoomLevel = 0;
   imageLoaded = true;
@@ -187,16 +224,16 @@ function readerZoom(mouseButton) {
   hideTooltip();
   if (mouseButton == 'leftClick') {
     zoomLevel++;
-    overlay.style.cursor = 'zoom-in'
+    overlay.style.cursor = 'zoom-in';
     if (zoomLevel >= zLevels.length) {
-      zoomLevel = 0
+      zoomLevel = 0;
     }
   } else if (mouseButton == 'rightClick') {
-    overlay.style.cursor = 'zoom-in'
+    overlay.style.cursor = 'zoom-in';
     if (zoomLevel == 0) {
-      overlayOff()
+      overlayOff();
     } else {
-      zoomLevel--
+      zoomLevel--;
     }
   }
   element.setAttribute('style', 'width:' + zLevels[zoomLevel] + '%');
@@ -215,7 +252,7 @@ function displayTooltip() {
     if (document.getElementById('tooltipSchubert') == null) {
       return;
     } else {
-      tooltip.parentNode.removeChild(tooltip)
+      tooltip.parentNode.removeChild(tooltip);
     }
   }, 15000);
   overlay.style.display = 'block';
@@ -238,7 +275,7 @@ var timeoutHandle = setTimeout(() => {
   } else {
     document.getElementById('dotContainerSchubert').style.display = 'block';
     document.getElementById('studentSchubert').style.display = 'block';
-    pageNumber.style.display = 'block'
+    pageNumber.style.display = 'block';
   }
 }, 500);
 
@@ -252,7 +289,7 @@ function hideElements() {
     document.getElementById('dotContainerSchubert').style.display = 'block';
     document.getElementById('studentSchubert').style.display = 'block';
     pageNumber.style.display = 'block';
-  }, 500)
+  }, 500);
 }
 
 // handles switching between pages
@@ -260,25 +297,25 @@ function plusSlides(n) {
   const currImage = document.getElementById('image_' + slideIndex);
   currImage.setAttribute('style', 'width:100%');
   zoomLevel = 0;
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
   overlay.scrollTo(0, 0); // return to top of the page
 }
 
 // used by dots to navigate pages
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 // adopted from slideshow turorial in W3C
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlidesSchubert');
-  var dots = document.getElementsByClassName("dotSchubert");
+  var dots = document.getElementsByClassName('dotSchubert');
   if (n > slides.length) {
-    slideIndex = 1
+    slideIndex = 1;
   }
   if (n < 1) {
-    slideIndex = slides.length
+    slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
@@ -288,5 +325,8 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' activeSchubert';
-  pageNumber.innerHTML = pageNumber.innerHTML.replace(/^[^\d]*(\d+)/, 'Page ' + slideIndex);
+  pageNumber.innerHTML = pageNumber.innerHTML.replace(
+    /^[^\d]*(\d+)/,
+    'Page ' + slideIndex
+  );
 }
